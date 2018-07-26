757870d8830c2fea35c8652e30ed37c1b47155de
Rajan Soni <rajan@administrators-macbook-pro-3.local>
2018-07-26 17:55:34 +0530

    aZ

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel : UILabel!
+    @IBOutlet var answerlabel : UILabel!
+    let questions : [String] = ["waht is 7+7?", "what is capital of vermont?", "what is cognac made from"]
+    let answers : [String] =  ["14", "montpelier", "graphs"]
+    var currentQuestionIndex : Int = 0
+    
+    @IBAction func showNwxtQuestion(_ sender : UIButton)
+    {
+        currentQuestionIndex += 1;
+        if currentQuestionIndex == questions.count
+        {
+            currentQuestionIndex = 0
+        }
+        let question : String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerlabel.text = "???"
+    
+    }
+    @IBAction func showAnswer(_ sender : UIButton)
+    {
+        let answer : String = answers[currentQuestionIndex]
+        answerlabel.text = answer
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+   
 
 
 }
